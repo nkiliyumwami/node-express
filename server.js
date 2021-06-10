8646e8d844e5f7afde602998de4e08220fc2165e
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-06-10 07:57:40 -0400

    Intro to Express Server part 2

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 //Create an express webserver 
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+//Using Morgan Middleware 
+app.use(morgan('dev'));
+
+//Set our server to serve static files
+app.use(express.static(__dirname + '/public'));
+
 app.use((req, res)=> {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
@@ -16,4 +22,7 @@ app.use((req, res)=> {
 //Use the server or listen to Server
 app.listen(port, hostname, ()=> {
     console.log(`Express Server running at http:// ${hostname}:${port}/`)
-})
+});
+
+
+
