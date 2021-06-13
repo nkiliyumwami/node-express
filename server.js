c5966ef03da8a09b8778c90b675c845339b08c0f
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-06-12 21:05:56 -0400

    Express Router Part 1

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //Create an express webserver 
+const e = require('express');
 const express = require('express');
 const morgan = require('morgan');
 
@@ -9,6 +10,57 @@ const app = express();
 
 //Using Morgan Middleware 
 app.use(morgan('dev'));
+
+//Using Json Middleware to parse json data 
+app.use(express.json());
+
+//Adding RestApi support endpoints routing method  
+app.all('/campsites',(req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+app.get('/campsites', (req, res)=> {
+    res.end('Will send all the campsites to you');
+})
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /campsites`);
+})
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+})
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+})
+
+
+
+
+
+
 
 //Set our server to serve static files
 app.use(express.static(__dirname + '/public'));
